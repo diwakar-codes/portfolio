80860369f7fa0b0aeeed4e0886a2baf9a3202e20
Diwakar Singh <chirags-mac@Diwakars-MacBook-Air.local>
2025-12-31 11:48:45 +0530

    Force recreate admin user on deploy

--- core/signals.py
+++ core/signals.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-import os
 
 User = get_user_model()
 
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
-    if User.objects.filter(username="admin").exists():
-        return
+    User.objects.all().delete()  # FORCE RESET (TEMPORARY)
 
     User.objects.create_superuser(
-        username="diwakarsingh",
-        email="rajputdiwakar2002@gmail.com",
-        password="Sungoddiwakar@2002"
+        username="admin",
+        email="admin@example.com",
+        password="Admin@123",
+        is_staff=True,
+        is_superuser=True
     )
